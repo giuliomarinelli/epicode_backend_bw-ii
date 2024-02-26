968e0bd82d4812f6781194348a177fed429de160
giuliomarinelli <giulio.marinelli@icloud.com>
2024-02-26 15:04:22 +0100

    lose work

---
+++ src/main/java/it/epicode/backend/bwii/epic_energy_services/Models/repositories/ProvinciaRepository.java
@@ -0,0 +1,8 @@
+package it.epicode.backend.bwii.epic_energy_services.Models.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProvinciaRepository {
+
+}
