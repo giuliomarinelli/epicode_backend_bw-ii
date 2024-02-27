2a449dd4108a36f839837256a8350ab17c9a471b
giuliomarinelli <giulio.marinelli@icloud.com>
2024-02-27 11:56:34 +0100

    rfix

--- src/main/java/it/epicode/backend/bwii/epic_energy_services/Controllers/ClienteController.java
+++ src/main/java/it/epicode/backend/bwii/epic_energy_services/Controllers/ClienteController.java
@@ -1,4 +1,4 @@
-package it.epicode.backend.bwii.epic_energy_services.Controllers;
+package it.epicode.backend.bwii.epic_energy_services.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
