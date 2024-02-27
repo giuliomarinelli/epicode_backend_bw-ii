383dcbe1ec15098900059e66a28f0e4fb10b07d5
samuelet23 <samueletoscano@hotmail.com>
2024-02-27 15:33:44 +0100

    Authorized method for role

--- src/main/java/it/epicode/backend/bwii/epic_energy_services/Controllers/ClienteController.java
+++ src/main/java/it/epicode/backend/bwii/epic_energy_services/Controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,7 @@ public class ClienteController {
         private ClienteService clienteSv;
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('USER,ADMIN')")
     public Page<Cliente> getAllCLiente(Pageable pageable){
         return clienteSv.getAll(pageable);
     }
@@ -35,12 +37,14 @@ public class ClienteController {
         return clienteSv.save(cliente);
     }
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Cliente updateCliente(@PathVariable UUID id, @RequestBody ClienteDTO cliente, BindingResult bindingResult)throws NotFoundException{
         HandlerException.notFoundException(bindingResult);
         return clienteSv.update(id, cliente);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteCliente(@PathVariable UUID id)throws NotFoundException{
         clienteSv.delete(id);
     }
--- src/main/java/it/epicode/backend/bwii/epic_energy_services/Controllers/FatturaController.java
+++ src/main/java/it/epicode/backend/bwii/epic_energy_services/Controllers/FatturaController.java
@@ -9,6 +9,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,18 +33,21 @@ public class FatturaController {
     }
 
     @PostMapping("/save")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Fattura saveFattura(@RequestBody FatturaDTO fattura, BindingResult bindingResult)throws NotFoundException{
         HandlerException.notFoundException(bindingResult);
         return fatturaSv.saveFacture(fattura);
     }
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Fattura updateFattura(@PathVariable int id, @RequestBody FatturaDTO fattura, BindingResult bindingResult) throws NotFoundException, BadRequestException {
         HandlerException.notFoundException(bindingResult);
         return fatturaSv.updateFacture(id, fattura);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteFattura(@PathVariable int id)throws NotFoundException{
         fatturaSv.deleteFacture(id);
     }
