065b50d24f89ead7cfae73a7d375d783cae9c3c4
zAeryos <dave_13_08@hotmail.it>
2024-02-27 10:52:03 +0100

    Repositories and filtering

---
+++ src/main/java/it/epicode/backend/bwii/epic_energy_services/repositories/IndirizzoRepository.java
@@ -0,0 +1,10 @@
+package it.epicode.backend.bwii.epic_energy_services.repositories;
+
+import it.epicode.backend.bwii.epic_energy_services.Models.entities.Indirizzo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface IndirizzoRepository extends JpaRepository<Indirizzo, UUID>, PagingAndSortingRepository<Indirizzo, UUID> {
+}
---
+++ src/main/java/it/epicode/backend/bwii/epic_energy_services/repositories/UtenteRepository.java
@@ -0,0 +1,10 @@
+package it.epicode.backend.bwii.epic_energy_services.repositories;
+
+import it.epicode.backend.bwii.epic_energy_services.Models.entities.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface UtenteRepository extends JpaRepository<Utente, UUID>, PagingAndSortingRepository<Utente, UUID> {
+}
