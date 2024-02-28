a698d2d906f594ca9eb5b2f260c689ce8c433808
giuliomarinelli <giulio.marinelli@icloud.com>
2024-02-28 22:50:58 +0100

    managed AuthService and ProfileService

--- src/main/resources/frontend/src/app/app.component.ts
+++ src/main/resources/frontend/src/app/app.component.ts
@@ -1,5 +1,10 @@
+import { UtenteDTO, iUtente } from './Models/i-utente';
 import { Component } from '@angular/core';
 import { Router, RoutesRecognized } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { on } from 'events';
 
 
 @Component({
@@ -8,18 +13,30 @@ import { Router, RoutesRecognized } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authSvc: AuthService) { }
 
   protected isHome: boolean = true
 
+  private userDTO: UtenteDTO = {
+    nome: 'Giulio',
+    cognome: 'Marinelli',
+    email: 'mariucscddioOoOoOOOOo@rossi.it',
+    password: 'abcdefg',
+    username: 'blablasvddnxskjnask'
+
+  }
+  private onlyOnce = true
   ngOnInit() {
     this.router.events.subscribe(e => {
       if (e instanceof RoutesRecognized) {
-        if (e.url !== '/' || !e.url ) {
+        if (e.url !== '/' || !e.url) {
           this.isHome = false
         }
       }
     })
+
+
   }
+
 
 }
--- src/main/resources/frontend/src/app/app.module.ts
+++ src/main/resources/frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration(),
