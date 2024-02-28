8c2a97937290e28cf00fa09e0c814a456e960459
samuelet23 <samueletoscano@hotmail.com>
2024-02-28 09:48:31 +0100

    Inizialized test JUnit

--- src/test/java/it/epicode/backend/bwii/epic_energy_services/EpicEnergyServicesApplicationTests.java
+++ src/test/java/it/epicode/backend/bwii/epic_energy_services/EpicEnergyServicesApplicationTests.java
@@ -1,13 +1,27 @@
 package it.epicode.backend.bwii.epic_energy_services;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class EpicEnergyServicesApplicationTests {
 
-	@Test
-	void contextLoads() {
+
+	static AnnotationConfigApplicationContext ctx;
+
+	@BeforeAll
+	static void openContext(){
+		ctx = new AnnotationConfigApplicationContext();
 	}
 
+
+
+	@AfterAll
+	static void closeContext(){
+		ctx.close();
+	}
 }
