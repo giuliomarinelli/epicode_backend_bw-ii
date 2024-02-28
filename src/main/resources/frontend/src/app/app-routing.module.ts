f8a518604cdd1e33c5266d04d796fbb2a2d7b8e3
samuelet23 <samueletoscano@hotmail.com>
2024-02-28 17:37:28 +0100

    aggiunti moduli per login e register

--- src/main/resources/frontend/src/app/app-routing.module.ts
+++ src/main/resources/frontend/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadChildren: () => import('./pages/documentazione/auth/home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule)
   },
-  { path: 'auth', loadChildren: () => import('./pages/documentazione/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'auth', loadChildren: () => import('./pages/documentazione/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'auth/register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) },
-  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }
+  {
+    path: 'register',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/auth/register/register.module').then(m => m.RegisterModule)
+  },
+
+
 ]
 
 @NgModule({
