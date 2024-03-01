73bbebd8cc891aceb15bdec7292531325d15de6a
andreacruciani9 <140162473+andreacruciani9@users.noreply.github.com>
2024-03-01 11:09:17 +0100

    controller Comuni/province

--- src/main/resources/frontend/src/app/pages/auth/login/login.module.ts
+++ src/main/resources/frontend/src/app/pages/auth/login/login.module.ts
@@ -5,16 +5,8 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
-export class LoginModule { }
+export class LoginModule {}
