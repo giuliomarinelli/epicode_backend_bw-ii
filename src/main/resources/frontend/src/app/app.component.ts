3c99abfae8e5433540f7d7964e140c83e6dfcb32
samuelet23 <samueletoscano@hotmail.com>
2024-03-01 08:56:18 +0100

    navbar dinamica in base alla pagina login o register

--- src/main/resources/frontend/src/app/app.component.ts
+++ src/main/resources/frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { UtenteDTO, iUtente } from './Models/i-utente';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { Router, RoutesRecognized } from '@angular/router';
+import { Router, RoutesRecognized, NavigationEnd, Event as NavigationEvent } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
+import { filter, firstValueFrom } from 'rxjs';
 import { on } from 'events';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -17,8 +17,9 @@ export class AppComponent {
   constructor(private router: Router, private authSvc: AuthService, @Inject(PLATFORM_ID) private platformId: string) { }
 
   protected isHome: boolean = true
-
   protected contentLoaded = false
+  protected isInLoginPage!:boolean;
+
 
   get isBrowserOnly(): boolean {
     return isPlatformBrowser(this.platformId);
@@ -33,6 +34,15 @@ export class AppComponent {
       }
     })
 
+    this.router.events
+    .pipe(filter((event: NavigationEvent): event is NavigationEnd => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      if (event.urlAfterRedirects.includes('/register')) {
+        this.isInLoginPage = false
+      } else {
+        this.isInLoginPage = true
+      }
+    });
 
   }
 
